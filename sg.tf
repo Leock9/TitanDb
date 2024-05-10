f35049b6d2cdea21284924fba231c89beedaa150
Leonardo Carvalho <42394625+Leock9@users.noreply.github.com>
2024-05-10 03:17:12 +0000

    liberando porta vpc

--- sg.tf
+++ sg.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-    ingress {
+  ingress {
     description = "Postgres"
     from_port   = 5432
     to_port     = 5432
