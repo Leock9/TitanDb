854b663520be2ffefc48fa0d6644536094538dd7
Leonardo Carvalho <42394625+Leock9@users.noreply.github.com>
2024-07-16 08:48:16 -0300

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -20,8 +20,8 @@ resource "aws_security_group" "sg" {
 
   ingress {
     description = "All"
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 5000
+    to_port     = 5000
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
