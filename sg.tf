17e9e6b0142b6d49429c0bab1eef4f41641ec480
Leonardo Carvalho <42394625+Leock9@users.noreply.github.com>
2024-07-16 10:02:57 -0300

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -11,7 +11,7 @@ resource "aws_security_group" "sg" {
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    description = "All"
+    description = "HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -19,7 +19,7 @@ resource "aws_security_group" "sg" {
   }
 
   ingress {
-    description = "All"
+    description = "Custom TCP Port 5000"
     from_port   = 5000
     to_port     = 5000
     protocol    = "tcp"
@@ -35,7 +35,7 @@ resource "aws_security_group" "sg" {
   }
 
   ingress {
-    description = "All"
+    description = "Custom TCP Port 30007"
     from_port   = 30007
     to_port     = 30007
     protocol    = "tcp"
@@ -43,7 +43,7 @@ resource "aws_security_group" "sg" {
   }
 
   ingress {
-    description = "VPC"
+    description = "VPC All Traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -51,21 +51,15 @@ resource "aws_security_group" "sg" {
   }
 
   ingress {
+    description = "All TCP/UDP Ports"
     from_port   = 0
     to_port     = 65535
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port   = 0
-    to_port     = 65535
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
- 
-  egress {
-    description = "All"
+    description = "All Outbound Traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
